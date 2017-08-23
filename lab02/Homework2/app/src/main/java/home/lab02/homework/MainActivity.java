c7c817819cc0c27faf619d12f191b95abafa321b
Student <student@LAB308-28.local>
2017-08-23 16:53:35 +0700

    sent homework

---
+++ lab02/Homework2/app/src/main/java/home/lab02/homework/MainActivity.java
@@ -0,0 +1,13 @@
+package home.lab02.homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_01);
+    }
+}
